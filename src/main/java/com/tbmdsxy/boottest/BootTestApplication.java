6f4cbbbde09fae81608177bd69f7d5539168645b
Wen <1132494612@qq.com>
2021-11-20 17:42:50 +0800

    idea-git-test

---
+++ src/main/java/com/tbmdsxy/boottest/BootTestApplication.java
@@ -0,0 +1,14 @@
+package com.tbmdsxy.boottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTestApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(BootTestApplication.class, args);
+        System.out.println("哈哈哈哈哈");
+    }
+
+}
